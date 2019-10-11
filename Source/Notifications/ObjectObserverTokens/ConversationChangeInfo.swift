2b8fc481e3123bd380b7ff76ad05b2a7e917591e
刘超 <chaosliu77@gmail.com>
2019-10-11 20:00:32 +0800

    top应用改变增加监听属性。

--- Source/Notifications/ObjectObserverTokens/ConversationChangeInfo.swift
+++ Source/Notifications/ObjectObserverTokens/ConversationChangeInfo.swift
@@ -53,6 +53,7 @@ extension ZMConversation : ObjectInSnapshot {
                     #keyPath(ZMConversation.creatorChangeTimestamp),
                     #keyPath(ZMConversation.selfRemark),
                     #keyPath(ZMConversation.apps),
+                    #keyPath(ZMConversation.topWebApps),
                     #keyPath(ZMConversation.communityID),
                     #keyPath(ZMConversation.isPlacedTop),
                     #keyPath(ZMConversation.isDisableSendMsg),
@@ -112,7 +113,7 @@ extension ZMConversation : ObjectInSnapshot {
     }
     
     public var appsChanged : Bool {
-        return changedKeysContain(keys: #keyPath(ZMConversation.apps))
+        return changedKeysContain(keys: #keyPath(ZMConversation.apps)) || changedKeysContain(keys: #keyPath(ZMConversation.topWebApps))
     }
 
     /// 新增对别人的回复类型改变
