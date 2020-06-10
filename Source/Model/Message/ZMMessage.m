ce576958f40d63a35b01a176e7c23bc0fc892bd3
李晓佳 <353243186@qq.com>
2020-06-10 11:04:00 +0800

    // 成员添加和移除,新入群消息不计入未读消息

--- Source/Model/Message/ZMMessage.m
+++ Source/Model/Message/ZMMessage.m
@@ -1366,14 +1366,15 @@ inManagedObjectContext:(NSManagedObjectContext * _Nonnull)moc
 - (BOOL)shouldGenerateUnreadCount;
 {
     switch (self.systemMessageType) {
-        case ZMSystemMessageTypeParticipantsRemoved:
-        case ZMSystemMessageTypeParticipantsAdded:
-        {
-            ZMUser *selfUser = [ZMUser selfUserInContext:self.managedObjectContext];
-            return [self.users containsObject:selfUser] && !self.sender.isSelfUser;
-        }
-        case ZMSystemMessageTypeNewConversation:
-            return !self.sender.isSelfUser;
+            // 成员添加和移除消息不计入未读消息
+//        case ZMSystemMessageTypeParticipantsRemoved:
+//        case ZMSystemMessageTypeParticipantsAdded:
+//        {
+//            ZMUser *selfUser = [ZMUser selfUserInContext:self.managedObjectContext];
+//            return [self.users containsObject:selfUser] && !self.sender.isSelfUser;
+//        }
+//        case ZMSystemMessageTypeNewConversation:
+//            return !self.sender.isSelfUser;
         case ZMSystemMessageTypeMissedCall:
         case ZMSystemMessageTypeServiceMessage:
             return YES;
